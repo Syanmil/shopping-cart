5b95d1c6ad56f0a0221cae61af99e01245379deb
Syanmil <syanmil@yahoo.com>
2017-02-09 23:29:51 +0700

    success creating new item from frontend using ajax on modal

--- server/routes/items.js
+++ server/routes/items.js
@@ -8,5 +8,8 @@ var itemController = require('../controller/itemController.js')
 router.post('/', itemController.create);
 router.get('/', itemController.findAll);
 router.put('/', itemController.updateStock);
+router.post('/tes', function(req, res){
+  res.send({body: req.body, file: req.file})
+})
 
 module.exports = router
